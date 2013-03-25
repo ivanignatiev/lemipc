2c5247cd3710a3acb93ae9925dc03cd6123bf817
ivan ignatiev <ignati_i@pc-ignati_i.(none)>
2013-03-25 15:31:39 +0100

    init commit, main function

---
+++ lemipc.c
@@ -0,0 +1,16 @@
+/*
+** lemipc.c for lemipc in /home/ignati_i//projects/lemipc
+** 
+** Made by ivan ignatiev
+** Login   <ignati_i@epitech.net>
+** 
+** Started on  Mon Mar 25 15:30:47 2013 ivan ignatiev
+** Last update Mon Mar 25 15:31:21 2013 ivan ignatiev
+*/
+
+#include <stdlib.h>
+
+int	main(int argc, char **argv)
+{
+  return (EXIT_SUCCESS);
+}
